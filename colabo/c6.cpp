40b8e4a4e56b02ceaddef0e0de91a8f305cac085
euncharm1ng <21700287@handong.edu>
2021-10-30 13:08:32 +0900

    c6 solved

--- colabo/c6.cpp
+++ colabo/c6.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
-
 using namespace::std;
-int n, card[50000], bigger[50000] = {0}, smaller[50000] = {0};
+
+int n;
+int card[50000], up[50000] ={0}, down[50000]={0}, upV[50000] = {0}, downV[50000] = {0};
+
+void
+calc(int start, int dir)
+{
+	int end = n, *arr = up, *val = upV, range = 1, chkr = 0;
+	if(start != 0){
+		end = -1; arr = down; val = downV;
+	}
+	arr[0] = card[start];
+	for(int i = start + dir; i != end; i += dir){
+		for(int j = 0; j < range; j++){
+			if(arr[j] >= card[i]){
+				arr[j] = card[i];
+				val[i] = j;
+				chkr = 1;
+				break;
+			}
+		}
+		if(!chkr){
+			arr[range] = card[i];
+			val[i] = range;
+			range++;
+		}
+		else chkr = 0;
+	}
+}
 int
 main()
 {
-	int currbig, currsmall,  ans =0;
+	int ans = 0;
 	scanf("%d", &n);
-	for(int i = 0; i < n; i++)
-		scanf("%d", &card[i]);
-	for(int i = n - 1; i > -1; i--){
-		currsmall = -1;
-		for(int j = i+ 1; j < n; j++){
-			if(card[i] > card[j] && smaller[j] > currsmall){
-				currsmall = smaller[j];
-			}
-		}
-		smaller[i] = currsmall + 1;
-	}
-	for(int i = 1; i < n; i++){
-		currbig = -1;
-		for(int j = 0; j < i; j++){
-			if(card[i] > card[j] && bigger[j] > currbig)
-				currbig = bigger[j];
-		}
-		bigger[i] = currbig + 1;
-	}
+	for(int i = 0; i < n; i++) scanf("%d", &card[i]);
+	calc(0, 1);
+	calc(n-1, -1);
 	for(int i = 0; i < n; i++){
-		printf("%3d %d %d \n", card[i], bigger[i], smaller[i]);
-		if(smaller[i] + bigger[i] + 1 > ans) ans = smaller[i] + bigger[i] + 1;
+		if(upV[i] + downV[i] + 1 > ans) ans = upV[i] + downV[i] + 1;
 	}
 	printf("%d\n", ans);
 }
-
