be41539aab6aca0ba43b237f181625275fa645d4
uja <21700287@handong.edu>
2021-11-02 21:54:04 +0900

    p6 solved

--- personal/p6.cpp
+++ personal/p6.cpp
@@ -1,60 +1,25 @@
 #include <iostream>
-
 using namespace::std;
 
-int n, cable[500];
+int n, cost[500][500] = {0}, cable[500];
 
-int
-calc(int* front, int* rear)
-{
-	int *r = rear, *l = front, rsum =*r, lsum = *l, sum = 0;
-	int *curr = front;
-/*
-	printf("\t arr: ");
-	while(curr <= rear){ 
-		sum+= *curr;
-		printf("%d ", *curr); 
-		curr++; 
-	}
-	puts("");
-	*/
-
-	if(front+1 == rear) return *front + *rear;
-	else if (front == rear) return 0;
-
-	while(1){
-		if(rsum > lsum){
-			l++;
-			lsum += *l;
-			sum -= *l;
-		}
-		else{
-			r--;
-			rsum += *r;
-			sum -= *r;
-		}
-		if(l+1 == r) break;
-		/*
-		if(l > r) {
-			printf("here:  %d %d\n", *l, *r);
-			break;
-		}
-		if(l > r || (rsum > sum && lsum > sum)) break;
-		*/
-	}
-	printf("here:  %d %d\n", *l, *r);
-	printf("lsum: %d, sum: %d, rsum: %d\n", lsum, sum, rsum);
-	return rsum + lsum + calc(front, l) + calc(r, rear);
-	/*
-	if(rsum > lsum) return rsum + lsum + sum + calc(front, r) + calc(r+1, rear);
-	else return rsum + lsum + sum + calc(front, l-1) + calc(l, rear);// + calc(l, r);
-	*/
-}
 int
 main()
 {
 	scanf("%d", &n);
 	for(int i = 0; i < n; i++) scanf("%d ", &cable[i]);
+	for(int i = 0; i < n-1; i++) cost[i+1][i] = cost[i][i+1] = cable[i] + cable[i+1];
+	
+	for(int i = 2; i < n; i++){
+		for(int j = 0; j < n - i; j++){
+			cost[j][j+i] = 	cost[j][j] + cost[j+1][j+i];
+			for(int k = 1; k < i; k++){
+				int temp = cost[j][j+k] + cost[j+k+1][j+i];
+				if(temp < cost[j][j+i]) cost[j][j+i] = temp;
+			}
+			cost[j][j+i] += cost[j+i][j] = cost[j+i-1][j] + cable[j+i];
+		}
+	}
 
-	printf("ans --> %d\n", calc(&cable[0], &cable[n-1]));
+	printf("%d\n", cost[0][n-1]);
 }
