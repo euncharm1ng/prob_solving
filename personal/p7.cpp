1f5bb5124ed55101f1a5fea87596ddb782ca8ff9
euncharm1ng <21700287@handong.edu>
2021-11-09 17:49:55 +0900

    working on p7

--- personal/p7.cpp
+++ personal/p7.cpp
@@ -13,12 +13,24 @@ ceiling(int a, int b)
 int
 bin_search(int bull)
 {
-	int loop = ceiling(m, bull);
-	printf("loop -> %d\n", loop);
-	for(int i = 0; i < loop; i++){
-		for(int j = i*bull; j < i*bull + bull; j++){
-			if(task[j] > i + d + 1) return -1;
+	/*
+	//printf("\n%d bulls:\n", bull);
+	for(int i = 0; i < n; i++){
+		//printf("\tday %d: ", i);
+		for(int j = 0; j < bull && i*bull+j < m; j++){
+			//printf("%d+%d ", task[i*bull+j], i*bull+j);
+			if(task[i*bull+j]+d <= i+1) return -1;
 		}
+		//puts("");
+	}
+	return 1;
+	*/
+
+	int day = 1, task_cnt = 0;
+	for(int i = 0; i < m; i++){
+		if(task[i]+d < day+1) return -1;
+		task_cnt++;
+		if(task_cnt == bull){task_cnt = 0; day++;}
 	}
 	return 1;
 }
@@ -26,22 +38,31 @@ bin_search(int bull)
 int 
 main()
 {
+	int mid;
 	scanf("%d %d %d", &n, &d, &m);
 
-	min_k = ceiling(m, n);
-	max_k = ceiling(m, d+1); // +1 or not?
+	min_k = ceiling(m, n) - 1;
+	max_k = ceiling(m, d) + 1; // +1 or not?
 
 	for(int i = 0; i < m; i++)
 		scanf("%d", &task[i]);
 	
 	sort(task, task+m);
 
-	for(int i = 0; i < m; i++)
-		printf("%d ", task[i]);
-	puts("");
-	printf("start with %d, max %d\n", min_k, max_k);
-	printf("with 1, result %d\n", bin_search(1));
-	printf("with 2, result %d\n", bin_search(2));
-	printf("with 3, result %d\n", bin_search(3));
-	printf("with 4, result %d\n", bin_search(4));
+	//for(int i = 0; i < m; i++) printf("%d ", task[i]);
+
+	while(min_k + 1 < max_k){
+		mid = (max_k + min_k) / 2;
+		printf("\nmin: %d, mid: %d, max: %d\n", min_k, mid, max_k);
+		if(bin_search(mid) == 1){
+			max_k = mid;
+			puts("return 1");
+		}
+		else{
+			min_k = mid;
+			puts("return -1");
+		}
+	}
+	//printf("%d\n", max_k);
+	printf("ans--> %d\n", max_k);
 }
