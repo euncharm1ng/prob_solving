2954131eab52de4cce245f56b9a0f8549ad2aba2
euncharm1ng <21700287@handong.edu>
2021-11-12 04:07:31 +0900

    working on c8

--- colabo/c8.cpp
+++ colabo/c8.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
-int n, d;
+int n, d, d_square;
 vector<pair<int, int> > vill;
+
+inline int
+calc_range(int y)
+{ return sqrt(d_square - pow(y, 2)); }
 
 int
 main()
 {
-	int a, b;
+	int a, b, range;
 	scanf("%d %d", &n, &d);
+	d_square = pow(d, 2);
 	for(int i = 0; i < n; i++){
 		scanf("%d %d", &a, &b);
-		vill.push_back({a, b});
+		range = calc_range(b);
+		vill.push_back(make_pair(a-range, a+range));
+	}
+
+	for(int i = 0; i < n; i++){
+		printf("from %d to %d\n", vill[i].first, vill[i].second);
 	}
 }
