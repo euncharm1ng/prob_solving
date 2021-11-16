966fbd0983ca097ce67fc33b5a74f69b8d90a734
uja <21700287@handong.edu>
2021-11-16 16:15:59 +0900

    p8 solved

--- personal/p8.cpp
+++ personal/p8.cpp
@@ -5,39 +5,28 @@
 using namespace std;
 typedef long long ll;
 
-int n, t;
-ll max_dist;
-vector<pair<int, ll> > folks;
-
 int
 main()
 {
-	int a; 
-	ll b;
+	int a, ans, n, t;
+	ll b, max_dist;
+	vector<pair<int, ll> > folks;
+
 	scanf("%d %d", &n, &t);
+	ans = n;
+
 	for(int i = 0; i < n; i++){
 		scanf("%d %lld", &a, &b);
-		folks.push_back({a, b * t + a});
+		folks.push_back({a, b*t+a});
 	}
 
 	sort(folks.begin(), folks.end(), greater<>());
 
-/*
-	for(int i = 0; i < n; i++){
-		printf("%d %lld\n", folks[i].first, folks[i].second);
-	}
-*/
-
 	max_dist = folks[0].second;
 	for(int i = 1; i < n; i++){
-		if(folks[i].second >= max_dist) n--;
+		if(folks[i].second >= max_dist) ans--;
 		else max_dist = folks[i].second;
 	}
 
-	//printf("ans --> %d\n", n);
-	printf("%d\n", n);
+	printf("%d\n", ans);
 }
-
-
-//1000000000 == 10^9
-//1000000000000000000
