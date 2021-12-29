c3de397b3169fa4d2578a7c19ab71ad976d3c671
uja <21700287@handong.edu>
2021-12-29 19:29:15 +0900

    1167 solved

--- baek/baek01167.cpp
+++ baek/baek01167.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
+#include <stack>
+#include <cstring>
 
 using namespace std;
+typedef pair<int, int> pii;
 
-int v, _a, _i;
-short _b;
-vector<pair<int, short> > tree[100001];
+int v, _a, _i, _b, next_start = -1;
+vector<pii> tree[100001];
+stack<pii> nodes;
 bool visited[100001];
 
 void
@@ -23,13 +25,13 @@ print_tree()
 }
 
 pair<int, int> // <with, without>
-do_bfs(int curr_node, int from_node)
+do_dfs(int curr_node, int from_node)
 {
 	int edge_cnt = tree[curr_node].size();
 	int max1 = 0, max2 = 0, without = 0;
 	for(int i = 0; i < edge_cnt; i++){
 		if(from_node != tree[curr_node][i].first){
-			pair<int, int> res = do_bfs(tree[curr_node][i].first, curr_node);
+			pii res = do_dfs(tree[curr_node][i].first, curr_node);
 			int curr_dist = res.first + tree[curr_node][i].second;
 			if(curr_dist > max1){
 				max2 = max1;
@@ -44,28 +46,50 @@ do_bfs(int curr_node, int from_node)
 	return make_pair(max1, max(without, max1 + max2));
 }
 
-void
-my_bfs(int curr_node)
+int
+my_dfs(int start)
 {
-	
+	int res = 0;
+	memset(visited, false, sizeof(false) * 100001);
+	nodes.push({start, 0});
+	visited[start] = true;
+	while(!nodes.empty()){
+		pii curr_pii = nodes.top();
+		nodes.pop();
+		int curr = curr_pii.first; 
+		int dist = curr_pii.second;
+		if(dist > res){
+			res = dist;
+			next_start = curr;
+		}
+		for(int i = 0; i < tree[curr].size(); i++){
+			int next = tree[curr][i].first;
+			if(!visited[next]){
+				nodes.push({next, dist+tree[curr][i].second});
+				visited[next] = true;
+			}
+		}
+	}
+	return res;
 }
 
 int
 main()
 {
-	memset(visited, false, sizeof(false) * 100001);
 	scanf("%d", &v);
 	for(int i = 1; i <= v; i++){
 		scanf("%d", &_i);
 		while(1){
 			scanf("%d", &_a);
 			if(_a != -1){
-				scanf("%hd", &_b);
+				scanf("%d", &_b);
 				tree[_i].push_back(make_pair(_a, _b));
 			}
 			else break;
 		}
 	}
-	pair<int, int> res = do_bfs(1, -1);
-	printf("%d\n", max(res.first, res.second));
+	//pii res = do_dfs(1, -1);
+	//printf("%d\n", max(res.first, res.second));
+	my_dfs(1);
+	printf("%d\n", my_dfs(next_start));
 }
