dd96cf4c35b4fd0a9492f7e6e1902a3c57cb9afc
euncharm1ng <21700287@handong.edu>
2022-01-17 01:07:27 +0900

    working on 235690

--- working/baek23569.cpp
+++ working/baek23569.cpp
@@ -2,21 +2,34 @@
 
 using namespace std;
 
+int n, m, _a, _b;
 int friendship[1001][1001];
-int inssa = 0;
 
 int 
 main()
 {
 	memset(friendship, 0, sizeof(int)*1001*1001);
 	scanf("%d %d", &n, &m);
-	for(int i = 0; i < n; i++){
+	for(int i = 0; i < m; i++){
 		scanf("%d %d", &_a, &_b);
-		if(_a < _b) friendship[_a][_b] = 1;
-		else friendship[_b][_a] = 1;
+		/*
+		friendship[_a][++friendship[_a][0]] = _b;
+		friendship[_b][++friendship[_b][0]] = _a;
+		*/
+		friendship[_a][_b] = 1;
+		friendship[_b][_a] = 1;
 	}
-
-	for(int i = 0; i < n; i++){
-
+	/*
+	for(int i = 1; i <= n; i++){
+		printf("%d ++ %d -> ", i, friendship[i][0]);
+		for(int j = 1; j <= friendship[i][0]; j++)
+			printf("%d ", friendship[i][j]);
+		puts("");
+	}
+	*/
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++)
+			printf("%d ", friendship[i][j]);
+		puts("");
 	}
 }
